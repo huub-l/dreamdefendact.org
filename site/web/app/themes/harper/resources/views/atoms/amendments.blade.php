777b067b8a5c500b53866b5f3457ed0ad31c0e34
Kelly Mears <code@kellymears.me>
2018-10-22 15:59:13 -0700

    style improvements

--- site/web/app/themes/harper/resources/views/atoms/amendments.blade.php
+++ site/web/app/themes/harper/resources/views/atoms/amendments.blade.php
@@ -9,7 +9,7 @@
 
      <div class="columns b2 b--black">
        <div class="title-wrapper column">
-         <h2 class="is-4 mt4-ns mt0 title is-1 white bg-dark-red">Amendment 1: Vote NO</h2>
+         <h2 class="is-4 title is-1">Amendment 1: Vote NO</h2>
        </div>
        <div class="column is-5">
          <div class="box">
@@ -90,22 +90,36 @@
                    <h2 class="is-4 title is-1 white bg-dark-red">Amendment 5: Vote NO</h2>
                  </div>
                  <div class="column is-5">
-                   <h2 class="title is-3">What Is It?</h2>
+              <div class="box">
+                <div class="content">
+                  <h2 class="card-title">What Is It?</h2>
                    <p>Amendment 5 requires a nearly impossible 2/3 legislative vote to end tax breaks to big corporations or for the state to pay for improving schools or emergency responses to weather, environmental crises or other vital programs.</p>
-                   <h2 class="title is-3">Why Vote No?</h2>
+</div></div>
+              <div class="box">
+                <div class="content">
+                  <h2 class="card-title">Why Vote No?</h2>
                    <p>If passed, this would make it more difficult to change tax laws and would handcuff the government's hands when it comes to making decisions in times of crisis. Big corporations must pay their share.</p>
                  </div>
+                </div>
+              </div>
                  {{-- halfway!  --}}
                  <div class="title-wrapper column is-1">
                    <h2 class="is-4 title is-1">Amendment 6: Vote NO</h2>
                  </div>
                  <div class="column is-5">
-                   <h2 class="title is-3">What Is It?</h2>
+                                 <div class="box">
+                <div class="content">
+                  <h2 class="card-title">What Is It?</h2>
                    <p>Amendment 6, also known as "Marsy's Law," removes constitutional protections for those accused of crimes and expands already existing protections for victims. Redefines "victim" in ways that will compromise the privacy of the victim who was injured, especially in domestic abuse and sexual assault cases. Allows for corporations/businesses to be considered victimes.</p>
-                   <h2 class="title is-3">Why Vote No?</h2>
+                    </div></div>
+                                 <div class="box">
+                <div class="content">
+                  <h2 class="card-title">Why Vote No?</h2>
                    <p>"Marsy's Law" is being bankrolled by a California millionaire who's pushing this "victim's bill of rights" into states' constitutions across the country. In Florida, the amendment is supported by Senator Lauren Brook, whose dad is a lobbyist with GEO Group, the largest private prison and immigrant detention company in the country. Lauren has taken tens of thousands in contributions from this company.</p>
                  </div>
                </div>
+              </div>
+            </div>
                {{-- end --}}
                {{-- begin D: --}}
                     <div class="columns b2 b--black">
@@ -113,21 +127,33 @@
                         <h2 class="is-4 title is-1 white bg-dark-red">Amendment 7: Vote NO</h2>
                       </div>
                       <div class="column is-5">
-                        <h2 class="title is-3">What Is It?</h2>
+                                      <div class="box">
+                <div class="content">
+                  <h2 class="card-title">What Is It?</h2>
                         <p>Amendment 7 requires universities' governing bodies to get supermajority approval from their members to increase student fees or impose new ones. Requires death benefits for first responders and military members, adds state college system structure to constitution.</p>
-                        <h2 class="title is-3">Why Vote No?</h2>
+                                      </div></div><div class="box">
+                <div class="content">
+                  <h2 class="card-title">Why Vote No?</h2>
                         <p>Supermajorities make it really hard in this polarized climate to make anything change. We also aren't a fan of Florida's college structure being cemented into the state constitution. This would also increase fees or taxes when family members of the military who die in line of service are already compensated by the government.</p>
+                      </div>
+                      </div>
                       </div>
                       {{-- halfway!  --}}
                       <div class="title-wrapper column is-1">
                         <h2 class="is-4 title is-1">Amendment 9: Vote YES</h2>
                       </div>
                       <div class="column is-5">
-                        <h2 class="title is-3">What Is It?</h2>
+                                      <div class="box">
+                <div class="content">
+                  <h2 class="card-title">What Is It?</h2>
                         <p>Amendment 9 protects Florida's coasts by banning oil drilling in Florida near shore waters. Bans vaping in enclosed indoor workspaces.</p>
-                        <h2 class="title is-3">Why Vote Yes?</h2>
+                        </div></div>              <div class="box">
+                <div class="content">
+                  <h2 class="card-title">Why Vote Yes?</h2>
                         <p>If passed, this would help protect Florida's coast and our oceans.</p>
                       </div>
+                    </div>
+                    </div>
                     </div>
                     {{-- end --}}
                     {{-- begin D: --}}
@@ -136,22 +162,30 @@
                              <h2 class="is-4 title is-1 white bg-dark-red">Amendment 10: Vote NO</h2>
                            </div>
                            <div class="column is-5">
-                             <h2 class="title is-3">What Is It?</h2>
+                                           <div class="box">
+                <div class="content">
+                  <h2 class="card-title">What Is It?</h2>
                              <p>Amendment 10 creates an Office of Domestic Security and Counter-terrorism within the Florida Department of Law Enforcement. Mandates the existence of a State Department of Veterans Affairs (even though the constitution already authorizes the Legislature to do so).</p>
-                             <h2 class="title is-3">Why Vote No?</h2>
+                             </div></div>              <div class="box">
+                <div class="content">
+                  <h2 class="card-title">Why Vote No?</h2>
                              <p>If passed, this gives police more power and forces all counties to elect and never abolish certain offices such as sheriff's office, county appraiser, etc.<</p>
-                           </div>
+                           </div></div></div>
                            {{-- halfway!  --}}
                            <div class="title-wrapper column is-1">
                              <h2 class="is-4 title is-1">Amendment 11: Vote YES</h2>
                            </div>
                            <div class="column is-5">
-                             <h2 class="title is-3">What Is It?</h2>
+                                           <div class="box">
+                <div class="content">
+                  <h2 class="card-title">What Is It?</h2>
                              <p>Amendment 11 REPEALS the following: a prohibition against non-residents owning, buying, and selling property; a requirement for a high-speed ground transportation system, and a provision that requires the state to prosecute criminal suspects under the law they were originally charged under, even if the law has changed.</p>
-                             <h2 class="title is-3">Why Vote No?</h2>
+                             </div></div>              <div class="box">
+                <div class="content">
+                  <h2 class="card-title">Why Vote No?</h2>
                              <p>If passed, citizens would be prosecuted under the most current laws on the books rather than earlier laws, i.e. marijuana laws.</p>
                            </div>
-                         </div>
+                         </div></div></div>
                          {{-- end --}}
                {{-- begin D: --}}
                     <div class="columns b2 b--black">
@@ -159,22 +193,30 @@
                         <h2 class="is-4 title is-1 white bg-dark-red">Amendment 12: Vote YES</h2>
                       </div>
                       <div class="column is-5">
-                        <h2 class="title is-3">What Is It?</h2>
+                                      <div class="box">
+                <div class="content">
+                  <h2 class="card-title">What Is It?</h2>
                         <p>Amendment 12 increases the amount of time that elected officials and government employees can wait before they can lobby the government from two to six years.</p>
-                        <h2 class="title is-3">Why Vote Yes?</h2>
+                        </div></div>              <div class="box">
+                <div class="content">
+                  <h2 class="card-title">Why Vote Yes?</h2>
                         <p>Politicians get paid to influence political decisions after they have left office. If passed, this would make it harder for them to use money to influence their former colleagues for private interest.</p>
-                      </div>
+                      </div></div></div>
                       {{-- halfway!  --}}
                       <div class="title-wrapper column is-1">
                         <h2 class="is-4 title is-1">Amendment 13: Vote YES</h2>
                       </div>
                       <div class="column is-5">
-                        <h2 class="title is-3">What Is It?</h2>
+                                      <div class="box">
+                <div class="content">
+                  <h2 class="card-title">What Is It?</h2>
                         <p>Phases out commercial greyhound racing by 2020.</p>
-                        <h2 class="title is-3">Why Vote Yes?</h2>
+                      </div></div><div class="box">
+                <div class="content">
+                  <h2 class="card-title">Why Vote Yes?</h2>
                         <p>If passed, this would phase out greyhound racing in the state of Florida and prevent the abuse of Greyhound dogs.</p>
                       </div>
-                    </div>
+                    </div></div></div>
                     {{-- end --}}
 
 
